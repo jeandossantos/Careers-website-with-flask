572b8aebc0165e06c851aa1541d9adc990d9918f
jeandossantos <40217619+jeandossantos@users.noreply.github.com>
2023-12-14 19:36:49 +0000

    Dynamic page to each job

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 from flask import Flask, jsonify, render_template
 
-from database.job_repository import get_jobs
+from database.job_repository import load_job_from_db, load_jobs_from_db
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-  jobs = get_jobs()
+  jobs = load_jobs_from_db()
   return render_template('home.html', jobs=jobs, company_name='Jovian')
+
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs = get_jobs()
+  jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+
+  if job is None:
+    return 'Not Founded', 404
+    
+  return render_template('job-page.html', job=job)
+
+#-------- API ROUTES ---------
+
+
+@app.route('/api/jobs/<id>')
+def get_job(id):
+  job = load_job_from_db(id)
+
+  return jsonify(job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=3001, debug=True)
--- database/job_repository.py
+++ database/job_repository.py
@@ -3,22 +3,32 @@ from sqlalchemy import text
 from .db_setup import engine
 
 
-def get_jobs():
-  jobs = []
+def parse_sqlachemy_row_to_dict(columns, row):
+  job = {}
+  # cria um dicionário com as colunas e seus valores
+  for col in range(len(columns)):
+    job[columns[col]] = row[col]
 
+  return job
+
+
+def load_jobs_from_db():
+   with engine.connect() as conn:
+    jobs = conn.execute(text("select * from jobs"))
+
+    return jobs
+
+
+def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
+    result = conn.execute(text("select * from jobs where id = :id"),
+                          {"id": id})
     result_all = result.all()
-    # cria uma lista com as colunas da tabela
+
+    if len(result_all) == 0:
+      return None
+
     columns = list(result.keys())
+    job = parse_sqlachemy_row_to_dict(columns, tuple(result_all[0]))
 
-    for row_values in result_all:
-      job = {}
-      # cria um dicionário com as colunas e seus valores
-      for col in range(len(columns)):
-        job[columns[col]] = row_values[col]
-      
-      # adiciona o dicionário à lista de jobs
-      jobs.append(job)
-
-  return jobs
+    return job
